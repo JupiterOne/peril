647eaf808ccd853b0335f6cc4c13e9dc52223c0a
Erich Smith <erich.smith@jupiterone.com>
2021-01-22 14:50:23 -0500

    'peril' CLI stub
    Use OCLIF: Open CLI Framework to stub out single command CLI binary
    that will use `ts-node` in dev mode, and the compiled JavaScript upon
    release.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-/*
- do something amazing
-*/
+import {Command, flags} from '@oclif/command'
 
-async function run() {
-  console.log('Module init');
+class Peril extends Command {
+  static description = 'describe the command here'
 
-  try {
-    //
-  } catch (e) {
-    console.error(e);
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {args, flags} = this.parse(Peril)
+
+    const name = flags.name ?? 'world'
+    this.log(`hello ${name} from ./src/index.ts`)
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
   }
 }
 
-run().catch((err) => {
-  console.error(
-    `Runtime error. ${err.stack || err.toString()}`
-  );
-  process.exit(1);
-});
+export = Peril
