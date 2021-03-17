646bfcd18db8c29ba681f645a526f95fcdc08482
Erich Smith <erich.smith@jupiterone.com>
2021-03-17 17:20:12 -0400

    Fix: support configurable ignore of indirect dependency vulns

--- src/code.test.ts
+++ src/code.test.ts
@@ -1,11 +1,21 @@
-import { parseGitDiffShortStat, getGitDiffStats, locCheck, filesChangedCheck, depScanCheck, parseShiftLeftDepScan } from './code';
-import { ShortStat } from './types';
+import { cloneDeep } from 'lodash';
 import { depScanFindings } from '../test/fixtures/depscanFindings';
 import { config } from '../test/fixtures/testConfig';
+import {
+  depScanCheck,
+  filesChangedCheck,
+  getGitDiffStats,
+  locCheck,
+  parseGitDiffShortStat,
+  parseShiftLeftDepScan,
+} from './code';
+import { ShortStat } from './types';
 
 describe('code risks', () => {
   it('parseGitDiffShortStat parses git diff stats for numeric values', () => {
-    const parsedOutput = parseGitDiffShortStat(' 9 files changed, 12 insertions(+), 43 deletions(-)');
+    const parsedOutput = parseGitDiffShortStat(
+      ' 9 files changed, 12 insertions(+), 43 deletions(-)'
+    );
     expect(parsedOutput.filesChanged).toEqual(9);
     expect(parsedOutput.linesAdded).toEqual(12);
     expect(parsedOutput.linesRemoved).toEqual(43);
@@ -17,15 +27,21 @@ describe('code risks', () => {
   });
 
   it('getGitDiffStats returns parsed git diff output with sentinel value', async () => {
-    const failedStats = await getGitDiffStats('main', jest.fn().mockResolvedValueOnce({
-      failed: true
-    }));
+    const failedStats = await getGitDiffStats(
+      'main',
+      jest.fn().mockResolvedValueOnce({
+        failed: true,
+      })
+    );
     expect(failedStats.filesChanged).toEqual(-1); // signal failure to caller
 
-    const stats = await getGitDiffStats('main', jest.fn().mockResolvedValueOnce({
-      failed: false,
-      stdout: ' 9 files changed, 12 insertions(+), 43 deletions(-)'
-    }));
+    const stats = await getGitDiffStats(
+      'main',
+      jest.fn().mockResolvedValueOnce({
+        failed: false,
+        stdout: ' 9 files changed, 12 insertions(+), 43 deletions(-)',
+      })
+    );
     expect(stats.filesChanged).toEqual(9);
     expect(stats.linesAdded).toEqual(12);
     expect(stats.linesRemoved).toEqual(43);
@@ -35,14 +51,14 @@ describe('code risks', () => {
     const stats1: ShortStat = {
       filesChanged: 1,
       linesAdded: 100,
-      linesRemoved: 0
+      linesRemoved: 0,
     };
     const risk = await locCheck(stats1, config);
     expect(risk.value).toEqual(1);
     const stats2: ShortStat = {
       filesChanged: 1,
       linesAdded: 300,
-      linesRemoved: 150
+      linesRemoved: 150,
     };
     const risk2 = await locCheck(stats2, config);
     expect(risk2.value).toEqual(1.5);
@@ -52,24 +68,34 @@ describe('code risks', () => {
     const stats1: ShortStat = {
       filesChanged: 10,
       linesAdded: 100,
-      linesRemoved: 100
+      linesRemoved: 100,
     };
     const risk = await filesChangedCheck(stats1, config);
     expect(risk.value).toEqual(0.5);
     const stats2: ShortStat = {
       filesChanged: 40,
       linesAdded: 300,
-      linesRemoved: 150
+      linesRemoved: 150,
     };
     const risk2 = await filesChangedCheck(stats2, config);
     expect(risk2.value).toEqual(2);
   });
 
   it('parseShiftLeftDepScan parses file of newline-delimited JSON strings into DepScanFinding[]', async () => {
-    const reportString = depScanFindings.map(f => JSON.stringify(f)).join('\n');
-    const findings = await parseShiftLeftDepScan('testReport', jest.fn().mockResolvedValueOnce(reportString));
+    const reportString = depScanFindings
+      .map((f) => JSON.stringify(f))
+      .join('\n');
+    const findings = await parseShiftLeftDepScan(
+      'testReport',
+      jest.fn().mockResolvedValueOnce(reportString)
+    );
     expect(findings).toEqual(depScanFindings);
-    expect(await parseShiftLeftDepScan('testReport', jest.fn().mockResolvedValueOnce(''))).toEqual([]);
+    expect(
+      await parseShiftLeftDepScan(
+        'testReport',
+        jest.fn().mockResolvedValueOnce('')
+      )
+    ).toEqual([]);
   });
 
   it('parseShiftLeftDepScan returns empty Array on error/missing report', async () => {
@@ -83,8 +109,18 @@ describe('code risks', () => {
   });
 
   it('depScanCheck ignores risk for <MEDIUM severity or unfixable findings', async () => {
-    const risk = await (depScanCheck(depScanFindings, config));
+    const konfig = cloneDeep(config);
+    konfig.values.checks.code.depscanFindings.ignoreIndirects = false;
+    const risk = await depScanCheck(depScanFindings, konfig);
     expect(risk.value).toEqual(7.5);
     expect(risk.description).toMatch(/1 HIGH/);
+  });
+
+  it('depScanCheck ignores risk for optional/indirect findings if ignoreIndirects is set', async () => {
+    const konfig = cloneDeep(config);
+    konfig.values.checks.code.depscanFindings.ignoreIndirects = true;
+    const risk = await depScanCheck(depScanFindings, konfig);
+    expect(risk.value).toEqual(0);
+    expect(risk.description).toMatch(/None/);
   });
 });
