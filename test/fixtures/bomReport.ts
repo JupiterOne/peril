89c45f899f2ada9156d8a3b263efd70a79fc4bf7
Neal Patel <neal.patel@jupiterone.com>
2021-04-26 18:08:16 -0400

    [P#21] adding missing fixture

---
+++ test/fixtures/bomReport.ts
@@ -0,0 +1,59 @@
+import { LicenseFinding } from './../../src/types';
+
+export const licenseFindings: LicenseFinding[] = [
+  {
+    purl: 'pkg:npm/babel/compat-data@7.13.15',
+    licenses: [
+      {
+        license: {
+          id: 'MIT',
+          url: 'https://opensource.org/licenses/MIT',
+        },
+      },
+    ],
+  },
+  {
+    purl: 'pkg:npm/caniuse-lite@1.0.30001208',
+    licenses: [
+      {
+        license: {
+          id: 'CC-BY-4.0',
+          url: 'https://opensource.org/licenses/CC-BY-4.0',
+        },
+      },
+    ],
+  },
+  {
+    purl: 'pkg:npm/electron-to-chromium@1.3.713',
+    licenses: [
+      {
+        license: {
+          id: 'ISC',
+          url: 'https://opensource.org/licenses/ISCq',
+        },
+      },
+    ],
+  },
+  {
+    purl: 'pkg:npm/babel/compat-data@7.13.15',
+    licenses: [
+      {
+        license: {
+          id: 'GPLv2',
+          url: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+        },
+      },
+    ],
+  },
+  {
+    purl: 'pkg:npm/electron-to-bromium@1.3.713',
+    licenses: [
+      {
+        license: {
+          id: 'MS-RL',
+          url: 'https://opensource.org/licenses/MS-RL',
+        },
+      },
+    ],
+  },
+];
